a75778056d3d3aac6e154f78aed297a5c88c9fa5
Philipp Frik <philipp.frik@qsc.de>
2020-11-17 10:35:17 +0100

    removed distro

--- manifests/init.pp
+++ manifests/init.pp
@@ -275,7 +275,7 @@ class network (
       content => template($network::hostname_file_template),
       notify  => $network::manage_config_file_notify,
     }
-    case $facts['os']['distro']['release']['major'] {
+    case $facts['os']['release']['major'] {
       '7': {
         exec { 'sethostname':
           command => "/usr/bin/hostnamectl set-hostname ${manage_hostname}",
